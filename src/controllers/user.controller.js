51461ce692e1309f12388c7c5cb6be3d5f4cc25d
Iqbal Fasri <iqbalx64@gmail.com>
2019-05-17 13:24:18 +0700

    User ES6 syntax, async/await and add middleware for validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,28 +1,85 @@
-const UserModel = require('../../models').users
+const UserModel = require("../../models").users;
 
 class UserController {
-  static all(req, res) {
-    UserModel.findAll().then(data => {
+  static async getAllUser(req, res) {
+    try {
+      const userData = await UserModel.findAndCountAll();
+
       res.json({
-        data: data,
-        total_data: countData(UserModel)
-      })
-    })
+        data: userData,
+        message: "success"
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: JSON.stringify(error)
+      });
+    }
   }
 
-  static getById(req, res) {
-    UserModel.findByPk(req.params.id).then(data => {
-      res.json({
-        data: data
-      })
-    })
+  static async getById(req, res) {
+    try {
+      let { id } = req.params;
+
+      const userById = await UserModel.findByPk(id);
+
+      res.status(200).json({
+        data: userById,
+        message: "success"
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: JSON.stringify(error)
+      });
+    }
   }
 
-  static store () {}
+  static async store(req, res) {
+    try {
+      let { username, email } = req.body;
 
-  static update () {}
+      let buildData = await UserModel.build({ username, email });
+      const saveData = await buildData.save();
 
-  static delete () {}
+      res.status(201).json({
+        data: saveData,
+        message: "success"
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error
+      });
+    }
+  }
+
+  static async update(req, res) {
+    try {
+      let { id } = req.params;
+
+      // const updateUser = await UserModel.update(
+      //   { username: req.body.username, email: req.body.email },
+      //   { where: { id: id } }
+      // );
+
+      return updateUser;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async delete(req, res) {
+    try {
+      let { id } = req.params;
+
+      await UserModel.destroy({ where: { id } });
+
+      res.json({
+        message: "delete success"
+      })
+
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,20 @@
-const Router = require('express').Router()
+const Router = require("express").Router();
 
 // user controller
-const { all, getById } = require('../controllers/user.controller')
+const User = require("../controllers/user.controller");
 
-Router.route('/').get(all).post()
-Router.route('/:id').get(getById)
+// middleware validation
+const {
+  schemaDataValidations,
+  validation
+} = require("../middleware/validation");
 
-module.exports = Router
+Router.route("/")
+  .get(User.getAllUser)
+  .post(validation(schemaDataValidations.users), User.store);
+Router.route("/:id")
+  .get(User.getById)
+  .put(User.update)
+  .delete(User.delete);
+
+module.exports = Router;
